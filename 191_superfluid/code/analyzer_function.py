77014386159a98d01d4db6977e9915ebb3362334
Ujini <eugene.ofriel@gmail.com>
2016-10-31 22:48:08 -0400

    added max voltage listing

--- 191_superfluid/code/analyzer_function.py
+++ 191_superfluid/code/analyzer_function.py
@@ -24,6 +24,10 @@ mmHg = cali_data[:, 0]
 V1   = cali_data[:, 1]
 V2   = cali_data[:, 2]
 
+plt.plot(mmHg, V1, 'ro')
+plt.plot(mmHg, V2, 'bo')
+plt.show()
+
 V3 = V2 / V1
 
 temperature = np.genfromtxt('temperature.txt')
@@ -45,13 +49,37 @@ a_run_03 = np.genfromtxt('a_run_03.txt')
 a_run_04 = np.genfromtxt('a_run_04.txt')
 
 def analyzer(data, form='b'):    
-    x = data[:, 2]
-    y = data[:, 0]
+    time = data[:, 2]
+    volt = data[:, 0]
     
-    plt.plot(x, y, form)
+    global_min = time.min()
+    global_max = time.max()    
+    
+    local_min = global_min
+    local_max = global_min + 0.485
+
+    max_volts  = []
+    
+    while local_max <= global_max:
+        local_list = []
+        for i in time:
+            if i >= local_min and i <= local_max:
+                ind      = list(time).index(i)
+                volt_val = data[ind, 0]
+                local_list.append(volt_val)
+        
+        max_value = max(local_list)
+        max_volts.append(max_value)
+        
+        local_min += 0.485
+        local_max += 0.485
+    
+    plt.plot(volt, time, form)
     plt.title('Title')
     plt.xlabel('Time (s)')
     plt.ylabel('INSERT LABEL (mV)')
     plt.show()
+    
+    return max_volts
     
 analyzer(a_run_02, 'g')
