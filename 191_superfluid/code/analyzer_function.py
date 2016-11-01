ab71e4283483f471be54bf94fbe464dab30ced38
Ujini <eugene.ofriel@gmail.com>
2016-11-01 11:48:15 -0400

    updated analyzer

--- 191_superfluid/code/analyzer_function.py
+++ 191_superfluid/code/analyzer_function.py
@@ -2,41 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+# Changes working directory to /data to access datasets
 os.chdir("../data/")
 
-"""
-import pandas as pd
-
-cali_data = pd.read_csv('cali_new.txt')
-
-mmHg = cali_data[[0]]
-V1   = cali_data[[1]]
-V2   = cali_data[[2]]
-
-plt.plot(mmHg, V1, 'ro')
-plt.plot(mmHg, V2, 'bo')
-plt.show()
-"""
-
+# Datasets
 cali_data = np.genfromtxt('cali_new.txt', delimiter=',', skip_header=1)
-
-mmHg = cali_data[:, 0]
-V1   = cali_data[:, 1]
-V2   = cali_data[:, 2]
-
-plt.plot(mmHg, V1, 'ro')
-plt.plot(mmHg, V2, 'bo')
-plt.show()
-
-V3 = V2 / V1
-
 temperature = np.genfromtxt('temperature.txt')
-
-temp_0 = temperature[:, 0]
-temp_1 = temperature[:, 1]
-
-plt.plot(temp_1, temp_0, 'g')
-plt.show()
 
 run_01 = np.genfromtxt('run_01.txt')
 run_02 = np.genfromtxt('run_02.txt')
@@ -48,18 +19,45 @@ a_run_02 = np.genfromtxt('a_run_02.txt')
 a_run_03 = np.genfromtxt('a_run_03.txt')
 a_run_04 = np.genfromtxt('a_run_04.txt')
 
+# Temperature (K) v. Pressure (mTorr) plot
+temp_0 = temperature[:, 0]
+temp_1 = temperature[:, 1]
+
+plt.plot(temp_1, temp_0, 'g')
+plt.show()
+
+# Calibration data plot
+mmHg = cali_data[:, 0]
+V1   = cali_data[:, 1]
+V2   = cali_data[:, 2]
+
+plt.plot(mmHg, V1, 'ro')
+plt.plot(mmHg, V2, 'bo')
+plt.show()
+
+#print("The ratio of V2:V1 without taking offset into account is: {} ").format(V2 / V1)
+
 def analyzer(data, form='b'):    
-    time = data[:, 2]
-    volt = data[:, 0]
+    
+    gamma = 0.1 # inital offset
+    delta = 0.485
+    
+    time = data[500:525, 2]
+    volt = data[500:525, 0]
     
     global_min = time.min()
     global_max = time.max()    
     
-    local_min = global_min
-    local_max = global_min + 0.485
+    local_min = global_min + gamma
+    local_max = local_min + delta
 
     max_volts  = []
-    
+ 
+'''   
+    time_list = list(time)
+    volt_list = list(volt)
+'''
+    plt.axvline(local_min)
     while local_max <= global_max:
         local_list = []
         for i in time:
@@ -71,15 +69,14 @@ def analyzer(data, form='b'):
         max_value = max(local_list)
         max_volts.append(max_value)
         
-        local_min += 0.485
-        local_max += 0.485
-    
-    plt.plot(volt, time, form)
+        plt.axvline(local_max)
+        local_min += delta
+        local_max += delta
+
+    plt.plot(time, volt, form)
     plt.title('Title')
     plt.xlabel('Time (s)')
     plt.ylabel('INSERT LABEL (mV)')
     plt.show()
-    
-    return max_volts
-    
-analyzer(a_run_02, 'g')
+
+analyzer(a_run_02, 'go')
