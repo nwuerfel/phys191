09de2e6b7d0e007ad5e72e8bc5c4638efae89378
Ujini <eugene.ofriel@gmail.com>
2016-11-05 15:21:00 -0400

    added PTconverter

--- 191_superfluid/code/analyzer_function.py
+++ 191_superfluid/code/analyzer_function.py
@@ -9,7 +9,7 @@ os.chdir("../data/")
 ### BEGIN DATASETS ###
 ######################
 '''
-SECOND SOUND DATA
+SECOND SOUND
 '''
 ss_run_01 = np.genfromtxt('ss_run_01.txt')
 ss_run_02 = np.genfromtxt('ss_run_02.txt')
@@ -21,7 +21,7 @@ ss_run_07 = np.genfromtxt('ss_run_07.txt')
 ss_run_08 = np.genfromtxt('ss_run_08.txt')
 
 '''
-HEAT CAPACITY DATA
+HEAT CAPACITY
 '''
 cali_data = np.genfromtxt('cali_new.txt', delimiter=',', skip_header=1)
 temperature = np.genfromtxt('temperature.txt')
@@ -35,10 +35,14 @@ run_05 = np.genfromtxt('run_05.txt')
 a_run_01 = np.genfromtxt('a_run_01.txt')
 a_run_02 = np.genfromtxt('a_run_02.txt')
 a_run_03 = np.genfromtxt('a_run_03.txt')
+
+"""
+# DO NOT USE THESE
 a_run_04 = np.genfromtxt('a_run_04.txt')
 a_run_05 = np.genfromtxt('a_run_05.txt')
 a_run_06 = np.genfromtxt('a_run_06.txt')
 a_run_07 = np.genfromtxt('a_run_07.txt')
+"""
 
 he_run_01 = np.genfromtxt('he_run_01.txt') 
 
@@ -54,7 +58,85 @@ a_l_a_run_01 = np.genfromtxt('he_above_l_a_run_01.txt')
 #######################
 ### BEGIN FUNCTIONS ###
 #######################
-  
+'''
+SECOND SOUND
+'''
+
+
+'''
+HEAT CAPCAITY
+'''
+def ppFinder(data):
+    
+    time = data[:, 2]
+    volt = data[:, 0]
+    
+
+    plt.plot(time, volt, 'g')
+    plt.title('Title')
+    plt.xlabel('Time (s)')
+    plt.ylabel('INSERT LABEL (mV)')
+    plt.show()
+
+    
+    time_list = list(time)
+    volt_list = list(volt)
+    
+    list_length = len(time_list)
+
+    plateau = []
+    comparison = 100
+    saved_point = 0
+    i = 0
+    
+    while i+1 <= list_length-1:
+        y1 = volt_list[i]
+        y2 = volt_list[i+1]
+            
+        working = abs((y1 - y2))
+            
+        if working <= comparison:
+            comparison = working
+            saved_point = y2
+        
+        if working > comparison:
+            plateau.append(saved_point)
+            comparison = 100
+        
+        i += 1
+    
+    return plateau
+
+def VPconverter(num):
+
+def PTconverter(num):
+    temp_mTorr = temperature[:, 0]
+    temp_mmHg  = temp_mTorr / 1000
+    temp_K     = temperature[:, 1]
+    
+    maxi_list = []
+    mini_list = []
+    
+    nlist = temp_mmHg - num
+    
+    for i in nlist:
+        if i > 0:
+            maxi_list.append(i)
+        else:
+            mini_list.append(i)
+    
+    P_max = min(maxi_list) + num
+    P_min = max(mini_list) + num
+    
+    temp_mmHg_list = list(temp_mmHg)
+    
+    K_max = temp_K[temp_mmHg_list.index(P_max)]
+    K_min = temp_K[temp_mmHg_list.index(P_min)]
+    
+    slope = (K_max - K_min) / (P_max - P_min)
+    
+    return P_min + slope * (num - P_min)
+    
 #######################
 #### END FUNCTIONS ####
 #######################
@@ -62,20 +144,23 @@ a_l_a_run_01 = np.genfromtxt('he_above_l_a_run_01.txt')
 ###################
 ### BEGIN PLOTS ###
 ###################
+'''
+SECOND SOUND
+'''
 
-###################
-#### END PLOTS ####
-###################
+'''
+HEAT CAPCITY
+'''
+# Temperature (K) v. Pressure (mTorr)
+temp_mTorr = temperature[:, 0]
+temp_mmHg  = temp_mTorr / 1000
+temp_K     = temperature[:, 1]
 
-
-
-# Temperature (K) v. Pressure (mTorr) plot
-temp_0 = temperature[:, 0]
-temp_1 = temperature[:, 1]
-
-plt.plot(temp_1, temp_0, 'g')
+plt.plot(temp_mmHg, temp_K, 'g')
 plt.show()
 
+# Calibration data plot
+'''
 # Calibration data plot
 mmHg = cali_data[:, 0]
 V1   = cali_data[:, 1]
@@ -86,6 +171,18 @@ plt.plot(mmHg, V2, 'bo')
 plt.show()
 
 #print("The ratio of V2:V1 without taking offset into account is: {} ").format(V2 / V1)
+'''
+###################
+#### END PLOTS ####
+###################
+
+'''
+'''
+'''
+JUNK
+'''
+'''
+'''
 
 def analyzer(data, form='b'):    
     
@@ -124,45 +221,3 @@ def analyzer(data, form='b'):
     plt.xlabel('Time (s)')
     plt.ylabel('INSERT LABEL (mV)')
     plt.show()
-
-def edgefinder(data):
-    
-    time = data[:25, 2]
-    volt = data[:25, 0]
-
-    plt.plot(time, volt, 'go')
-    plt.title('Title')
-    plt.xlabel('Time (s)')
-    plt.ylabel('INSERT LABEL (mV)')
-    plt.show()
-    
-    time_list = list(time)
-    volt_list = list(volt)
-    volt_list.reverse()
-    
-    list_length = len(time_list)
-
-    edge = []
-    comparison = 0
-    saved_point = 0
-    i = 0
-    
-    while i+1 <= list_length-1:
-        y1 = volt_list[i]
-        y2 = volt_list[i+1]
-            
-        working = abs((y1 - y2))
-            
-        if working >= comparison:
-            comparison = working
-            saved_point = y2
-        
-        if working < comparison:
-            edge.append(saved_point)
-            comparison = 0
-        
-        i += 1
-    
-    return edge
-
-print edgefinder(a_run_02)
